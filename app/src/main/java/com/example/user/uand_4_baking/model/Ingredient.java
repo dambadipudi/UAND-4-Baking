e5d7b69bcc09cdfffa4b44ae49a4b30d690fc836
Divya Ambadipudi <divya.shastry#gmail.com>
2018-07-16 15:50:02 -0700

    Closes #2: Added Retrofit to get Recipe data and Gson to parse the JSON

---
+++ app/src/main/java/com/example/user/uand_4_baking/model/Ingredient.java
@@ -0,0 +1,38 @@
+package com.example.user.uand_4_baking.model;
+
+public class Ingredient {
+
+    private float quantity;
+
+    private String measure;
+
+    private String ingredient;
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String toString() {
+        return "quantity=" + quantity + ", measure=" + measure + ", ingredient=" + ingredient;
+    }
+}
