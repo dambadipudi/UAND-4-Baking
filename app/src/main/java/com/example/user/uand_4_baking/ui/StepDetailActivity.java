4f56b83c16422c4433075d1555ffe5f617db508c
Divya Ambadipudi <divya.shastry#gmail.com>
2018-08-07 23:49:38 -0700

    Fixes: #14: Saves step position for configuration changes

--- app/src/main/java/com/example/user/uand_4_baking/ui/StepDetailActivity.java
+++ app/src/main/java/com/example/user/uand_4_baking/ui/StepDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.user.uand_4_baking.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -16,6 +17,10 @@ public class StepDetailActivity extends AppCompatActivity {
 
     private static String CLICKED_STEP_POSITION = "STEP_POSITION";
 
+    private static String CURRENT_STEP_POSITION = "CURRENT_STEP_POSITION";
+
+    StepDetailFragment currentStepFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +33,16 @@ public class StepDetailActivity extends AppCompatActivity {
             ArrayList<Step> stepList = intent.getParcelableArrayListExtra(CLICKED_STEP_LIST);
             int position = 0;
 
-            if(intent.hasExtra(CLICKED_STEP_POSITION)) {
-                position = intent.getIntExtra(CLICKED_STEP_POSITION, 0);
+            if(savedInstanceState == null) {
+                if (intent.hasExtra(CLICKED_STEP_POSITION)) {
+                    position = intent.getIntExtra(CLICKED_STEP_POSITION, 0);
+                }
+            }
+            else if(savedInstanceState.containsKey(CURRENT_STEP_POSITION)){
+                position = savedInstanceState.getInt(CURRENT_STEP_POSITION);
             }
 
-            StepDetailFragment currentStepFragment = new StepDetailFragment();
+            currentStepFragment = new StepDetailFragment();
 
             currentStepFragment.setStepData(stepList, position);
 
@@ -42,5 +52,11 @@ public class StepDetailActivity extends AppCompatActivity {
                     .add(R.id.step_detail_container, currentStepFragment)
                     .commit();
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(CURRENT_STEP_POSITION, currentStepFragment.getPosition());
     }
 }
